51aca27431ebbd5e667427830cd39317040d43aa
Fauve Wevers <fauve.wevers@uwv.nl>
2026-01-12 13:05:20 +0100

    Course info step 2

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -1,17 +1,20 @@
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercise}
+    </p>
+  )
+}
 const Header = (props) => {
   return <h1>{props.course}</h1>
 }
+
 const Content = (props) => {
-  return (<div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+  return (
+    <div>
+      <Part part={props.part1} exercise={props.exercises1}/>
+      <Part part={props.part2} exercise={props.exercises2}/>
+      <Part part={props.part3} exercise={props.exercises3}/>
     </div>
   )
 }
@@ -33,8 +36,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
-      <Total exercise1={exercises1} exercise2={exercises2} exercise3={exercises3}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Total exercise1={exercises1} exercise2={exercises2} exercise3={exercises3} />
     </div>
   )
 }
