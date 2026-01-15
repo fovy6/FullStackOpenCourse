138eaa6c5dca5df6e47b449aa9f231c6a5506a27
Fauve Wevers <fauve.wevers@uwv.nl>
2026-01-15 16:46:50 +0100

    Unicafe step 4

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -17,13 +17,20 @@ const Statistics = ({good, neutral, bad}) => {
   const average = (good * 1 + neutral * 0 + bad * -1) / all
   const positive = (good / all) * 100 + ' %'
 
-  return (
-    <div>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive}</p>
-    </div>
-  )
+  if (all === 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+    return (
+      <div>
+        <p>all {all}</p>
+        <p>average {average}</p>
+        <p>positive {positive}</p>
+      </div>
+    )
 }
 
 const App = () => {
