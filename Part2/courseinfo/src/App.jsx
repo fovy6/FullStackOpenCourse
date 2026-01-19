6bf58afa1f1f65caa106f4eab97ac68902c0880a
Fauve Wevers <fauve.wevers@uwv.nl>
2026-01-19 15:12:22 +0100

    Course info step 10

--- Part2/courseinfo/src/App.jsx
+++ Part2/courseinfo/src/App.jsx
@@ -1,48 +1,4 @@
-const Header = ({course}) => <h2>{course}</h2>
-
-const Part = ({part}) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-)
-
-const Content = ({parts}) => {
-  const result = parts.map(part => 
-      <Part key={part.id} part={part} />
-  )
-  return(
-    <div>
-      {result}
-    </div>
-  )
-}
-
-const Total = ({parts}) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-
-  return(
-    <b>total of {total} exercises</b>
-  )
-}
-
-const Course = ({course}) => (
-    <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-    </div>
-)
-
-const Courses = ({courses}) => {
-  const result = courses.map(course =>
-    <Course key={course.id} course={course}/>
-  )
-  return(
-    <div>
-       {result}
-    </div>
-  )
-}
+import Courses from './components/Courses'
 
 const App = () => {
   const courses = [
