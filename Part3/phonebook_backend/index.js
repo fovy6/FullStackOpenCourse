3675b75f1ed3acaab424f8cc71bc498186c331ea
Fauve Wevers <fauve.wevers@uwv.nl>
2026-01-23 14:59:49 +0100

    Phonebook backend step 8

--- Part3/phonebook_backend/index.js
+++ Part3/phonebook_backend/index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('content', (request, response) => {
+    return JSON.stringify(request.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 let persons = [
     { 
