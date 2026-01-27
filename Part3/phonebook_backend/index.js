837bbf1caf9e2fc9878fc2659328ecf11214c9c3
Fauve Wevers <fauve.wevers@uwv.nl>
2026-01-27 10:45:50 +0100

    Phonebook database step 4

--- Part3/phonebook_backend/index.js
+++ Part3/phonebook_backend/index.js
@@ -8,20 +8,24 @@ const app = express()
 app.use(express.json())
 app.use(express.static('dist'))
 
+// Use morgan middleware for logging
 morgan.token('content', (request, response) => {
     return JSON.stringify(request.body)
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
     Person
         .find({})
         .then(persons => {
             response.json(persons)
         })
+        .catch(error => {
+            next(error)
+        })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
     Person
     .findById(id)
@@ -29,43 +33,33 @@ app.get('/api/persons/:id', (request, response) => {
         if (person) {
             response.json(person)
         } else {
-            response.statusMessage = "No person found with this ID."
             response.status(404).end()
         }
     })
     .catch(error => {
-        console.log(error)
-        response.status(400).send({ error: 'Malformatted id' })
+        next(error)
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
     Person
     .findByIdAndDelete(id)
-    .then(person => {
-        if (person) {
-            response.statusMessage = "Person successfully deleted."
-            response.status(204).end()
-        } else {
-            response.statusMessage = "No person found with this ID."
-            response.status(404).end()
-        } 
+    .then(result => {
+        response.statusMessage = "Person successfully deleted."
+        response.status(204).end()
     })
     .catch(error => {
-        console.log(error)
-        response.status(400).send({ error: 'Malformatted id' })
+        next(error)
     })
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
-    if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'Name or number is missing.' 
-    })
-    }
+    if (!body.name || !body.number) (
+        next(error) 
+    )
     
     const newID = Math.floor(Math.random() * 10000)
     const newPerson = new Person({
@@ -79,7 +73,7 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.get('/info', async (request, response) => {
+app.get('/info', async (request, response, next) => {
     try{
         const persons = await Person.find({})
         const number_of_persons = persons.length
@@ -88,10 +82,28 @@ app.get('/info', async (request, response) => {
 
         response.send(htmlResponse)
     } catch (error) {
-        console.log(error)
-        response.status(500).end()
+        next(error)
     }
 })
+
+// handler of requests with unknown endpoint
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+// handler of requests with result to errors
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'Malformatted id' })
+    } else if (error.name === 'ReferenceError') {
+        return response.status(400).send({ error: 'Missing name or number' })
+    }
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
