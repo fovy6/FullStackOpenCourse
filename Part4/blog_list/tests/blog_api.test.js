df8368888b6b15c6fa7d1f9fbb81c1b7cfa4809b
Fauve Wevers <fauve.wevers@uwv.nl>
2026-02-03 10:34:17 +0100

    Blog list test step 2

--- Part4/blog_list/tests/blog_api.test.js
+++ Part4/blog_list/tests/blog_api.test.js
@@ -19,6 +19,14 @@ test('correct number of blogs is returned', async () => {
   assert.strictEqual(response.body.length, 2)
 })
 
+test('id property is named correctly', async () => {
+  const response = await api.get('/api/blogs')
+
+  response.body.forEach((blog) => {
+    assert.ok(blog.id)
+  })
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
