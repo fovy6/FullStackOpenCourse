935543b1f2a6490afb37351d0431b5a8233167fb
Fauve Wevers <fauve.wevers@uwv.nl>
2026-02-16 17:25:49 +0100

    Blog list expansions step 9

--- Part4/blog_list/controllers/blogs.js
+++ Part4/blog_list/controllers/blogs.js
@@ -44,7 +44,21 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const blog = await Blog.findById(request.params.id)
+  if (!blog) {
+    return response.status(404).json({ error: 'Blog not found' })
+  }
+  if (blog.user.toString() !== decodedToken.id.toString()) {
+    return response.status(401).json({ error: 'Unauthorized' })
+  }
+  await blog.deleteOne()
+  await User.findByIdAndUpdate(decodedToken.id, { $pull: { blogs: blog._id } })
+
   response.status(204).end()
 })
 
