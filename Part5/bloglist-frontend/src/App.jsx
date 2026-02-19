843f2e765ad82702f1943c58ed99934018f3b6a1
Fauve Wevers <fauve.wevers@uwv.nl>
2026-02-19 13:39:50 +0100

    Blog list frontend step 2

--- Part5/bloglist-frontend/src/App.jsx
+++ Part5/bloglist-frontend/src/App.jsx
@@ -19,10 +19,23 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
       const user = await loginService.login({ username, password })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -32,6 +45,12 @@ const App = () => {
         setErrorMessage(null)
       }, 5000)
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    blogService.setToken(null)
+    setUser(null)
   }
 
   const loginForm = () => (
@@ -78,7 +97,10 @@ const App = () => {
       {user && (
         <div>
           <h2>blogs</h2>
-          <p>{user.name} logged in</p>
+          <p> 
+            {user.name} logged in
+            <button onClick={handleLogout}>logout</button>
+          </p>
         </div>
       )}
       {user && showAllBlogs()}
