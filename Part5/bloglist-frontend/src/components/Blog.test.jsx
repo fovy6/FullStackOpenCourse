af430adfd78b1796a3d9f9d83bbdb2ceea11547a
Fauve Wevers <fauve.wevers@uwv.nl>
2026-03-18 13:20:54 +0100

    Blog list tests step 2

--- Part5/bloglist-frontend/src/components/Blog.test.jsx
+++ Part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 import { expect } from 'vitest'
 
@@ -24,4 +25,25 @@ test('renders title and author, but not url or likes by default', () => {
   expect(authorElement).toBeDefined()
   expect(urlElement).toBeNull()
   expect(likesElement).toBeNull()
+})
+
+test('renders url and likes when the view button is clicked', async () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'Full Stack Open',
+    url: 'https://fullstackopen.com/en/part5',
+    likes: 10,
+    user: {
+      name: 'Test User'
+    }
+  }
+
+  render(<Blog blog={blog} setBlogs={() => {}} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  expect(screen.getByText('https://fullstackopen.com/en/part5')).toBeDefined()
+  expect(screen.getByText('likes 10')).toBeDefined()
 })
